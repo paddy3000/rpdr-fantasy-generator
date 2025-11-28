ced5325ee7e6897b5c24306e7e9d5eb4f4763bab
paddy3000 <paddyohara1998@hotmail.co.uk>
2025-11-28 16:59:30 +0000

    basic functionality for creating list of episodes added

--- control.js
+++ control.js
@@ -18,10 +18,22 @@ const render=(function () {
         const queenListDiv = document.createElement("div");
         queenListDiv.id="queen-list-div";
 
+
+        const episodeFormDiv = document.createElement("div");
+        episodeFormDiv.id="episode-form-div";
+
+        const episodeListDiv = document.createElement("div");
+        episodeListDiv.id="episode-list-div";
+
         document.body.appendChild(queenFormDiv)
         document.body.appendChild(queenListDiv);
+        document.body.appendChild(episodeFormDiv)
+        document.body.appendChild(episodeListDiv);
 
+        addQueen();
+        addEpisode();
     }
+    
     const addQueen = function () {
         const queenForm = document.createElement("form");
         queenForm.id = "queen-form";
@@ -49,32 +61,68 @@ const render=(function () {
         console.log("render.addQueen: Queen input form created");
     }
 
-    const displayQueens = (function (queen, id) {
-            const queenDiv = document.createElement("div");
+    const addEpisode = function () {
+        const episodeForm = document.getElementById("episode-form-div");
+
+        const addEpisodeDiv = document.createElement("div");
+        addEpisodeDiv.id = "add-episode-div";
+
+        const episodeDataList = document.createElement("datalist");
+        episodeDataList.id = "episode-suggestions";
+
+        const addOption = function (optionName) {
+            const optionElement = document.createElement("option");
+            optionElement.value = optionName;
+            episodeDataList.appendChild(optionElement);
+        }
+
+        competitionData.episodeSuggestions.forEach(suggestion => {
+            addOption(suggestion)
+        });
+
+        const episodeInput = document.createElement("input");
+        episodeInput.id = "add-episode-input";
+        episodeInput.name = "add-episode-input";
+        episodeInput.setAttribute("list", "episode-suggestions");
+
+
+        const confirmButton = document.createElement("button");
+        confirmButton.id = "confirm-episode-button";
+        confirmButton.textContent = "Add";
+
+        addEpisodeDiv.appendChild(episodeDataList);
+        addEpisodeDiv.appendChild(episodeInput);
+        addEpisodeDiv.appendChild(confirmButton);
+        episodeForm.appendChild(addEpisodeDiv);
+    }
+
+    const displayElement = (function (type, name, id) {
+            const elementDiv = document.createElement("div");
             // queenDiv.id = album.id;
-            queenDiv.classList = "queen-list-element";
-            queenDiv.id = `queen-list-element-${id}`;
-            queenDiv.innerText = queen;
+            elementDiv.classList = `${type}-list-element`;
+            elementDiv.id = `${type}-list-element-${id}`;
+            elementDiv.innerText = name;
 
-            const removeButton = document.createElement("button");
-            removeButton.classList = "queen-list-remove";
-            removeButton.id = `queen-list-remove-${id}`;
-            removeButton.innerText = "x";
+            const removeButton = document.createElement(`button`);
+            removeButton.classList = `${type}-list-remove`;
+            removeButton.id = `${type}-list-remove-${id}`;
+            removeButton.innerText = `x`;
 
-            queenDiv.appendChild(removeButton);
+            elementDiv.appendChild(removeButton);
 
-            const queenListDiv = document.getElementById("queen-list-div");
-            queenListDiv.appendChild(queenDiv);
-            console.log("render.displayQueens: ", queen, " added");
+            const elementListDiv = document.getElementById(`${type}-list-div`);
+            elementListDiv.appendChild(elementDiv);
+            console.log(`render.displayElements: `, name, ` added to ${type} list`);
     })
 
-    return {init, addQueen, displayQueens};
+    return {init, addQueen, displayElement};
 })();
 
 const control=(function() {
     let queenID = 0;
+    let episodeID = 0;
 
-    const queenEnter = function() {
+    const queenAdd = function() {
         const queenForm = document.getElementById("queen-form");
         const addQueenInput = document.getElementById("add-queen-input");
 
@@ -85,7 +133,7 @@ const control=(function() {
                 const queen = {name: addQueenInput.value, id: queenID++};
                 addQueenInput.value = "";
                 competitionData.queens.push(queen);
-                render.displayQueens(queen.name, queen.id);
+                render.displayElement("queen", queen.name, queen.id);
                 console.log(`control.queenForm: List of queens is ${competitionData.queens.map(q => q.name).join(", ")}`)
                 return {queen}
             }
@@ -113,16 +161,36 @@ const control=(function() {
         // });
     }
 
-    return {queenEnter, queenListRemove}
+    const episodeAdd = function() {
+        const episodeFormDiv = document.getElementById("episode-form-div");
+        const addEpisodeInput = document.getElementById("add-episode-input");
+        const addEpisodeButton = document.getElementById("confirm-episode-button");
+        
+        addEpisodeButton.addEventListener("click", () => {
+            if (addEpisodeInput.value) {
+                const episode = {name: addEpisodeInput.value, id: episodeID++};
+                render.displayElement("episode", episode.name, episode.id);
+                addEpisodeInput.value = "";
+                competitionData.episodes.push(episode);
+                console.log(`control.episodeAdd: List of episodes is ${competitionData.episodes.map(q => q.name).join(", ")}`)
+                return {episode}
+            }
+        })
+    }
+
+    return {queenAdd, queenListRemove, episodeAdd}
 })();
 
 const competitionData = (function () {
     const queens = [];
 
-    return {queens}
+    const episodeSuggestions = ["Acting", "Ball", "Commerical", "Design", "Girl Group", "Improv", "Makeover", "Roast", "Rusical", "Snatch Game", "Stand-up", "Talent Show"];
+    const episodes = [];
+
+    return {queens, episodes, episodeSuggestions}
 })()
 
 render.init();
-render.addQueen();
-control.queenEnter();
+control.queenAdd();
 control.queenListRemove();
+control.episodeAdd();
